e436ea9e3539f79a9cf77af675d22afd4d0495ce
Jorge Miguel Angel <mig.jmaj@gmail.com>
2020-10-04 23:10:57 -0500

    First Commit

---
+++ js/app.js
@@ -0,0 +1,140 @@
+const container = document.querySelector('.container');
+const resultado = document.querySelector('#resultado');
+const formulario = document.querySelector('#formulario');
+
+window.addEventListener('load', () => {
+  formulario.addEventListener('submit', buscarClima);
+});
+
+function buscarClima(e) {
+  e.preventDefault();
+  console.log('buscando el clima');
+
+  //validar
+  const ciudad = document.querySelector('#ciudad').value;
+  const pais = document.querySelector('#pais').value;
+
+  if (ciudad === '' || pais === '') {
+    mostrarError('Ambos campos son obligatorios');
+    return;
+  }
+  //Consultar API
+  consultarApi(ciudad, pais);
+}
+
+function mostrarError(mensaje) {
+  const alerta = document.querySelector('.bg-red-100');
+  if (!alerta) {
+    const alerta = document.createElement('div');
+
+    alerta.classList.add(
+      'bg-red-100',
+      'border-red-400',
+      'text-red-700',
+      'px-4',
+      'py-3',
+      'rounded',
+      'max-w-md',
+      'mx-auto',
+      'mt-6',
+      'text-center'
+    );
+
+    alerta.innerHTML = `
+    <strong class="fond-bold">Error</strong>
+    <spam class="block">${mensaje}</span>
+    `;
+    container.appendChild(alerta);
+
+    setTimeout(() => {
+      alerta.remove();
+    }, 3000);
+  }
+}
+
+function consultarApi(ciudad, pais) {
+  const appId = 'b979089fc98fd2853ee9ec65a4fc42af';
+
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+
+  Spinner();
+  fetch(url)
+    .then((respuesta) => respuesta.json())
+    .then((datos) => {
+      limpiarHTML();
+      if (datos.cod === '404') {
+        mostrarError('Cuidad no encontrada');
+        return;
+      }
+      //impreme respuesta del html
+      mostrarCliema(datos);
+    });
+}
+
+function mostrarCliema(datos) {
+  const {
+    name,
+    main: { temp, temp_max, temp_min },
+  } = datos;
+
+  const centigrados = kelvinACentigrados(temp);
+  const min = kelvinACentigrados(temp_max);
+  const max = kelvinACentigrados(temp_min);
+  const nombreCiudad = document.createElement('p');
+  nombreCiudad.textContent = `Clima en ${name}`;
+  nombreCiudad.classList.add('font-bold', 'text-2xl');
+
+  const actual = document.createElement('p');
+  actual.innerHTML = `
+  ${centigrados} &#8451;
+  `;
+  actual.classList.add('font-bold', 'text-6xl');
+
+  const tempMaxima = document.createElement('p');
+  tempMaxima.innerHTML = `Max: ${max} &#8451; `;
+  tempMaxima.classList.add('text-xl');
+
+  const tempMin = document.createElement('p');
+  tempMin.innerHTML = `Min: ${min} &#8451; `;
+  tempMin.classList.add('text-xl');
+
+  const resultadoDiv = document.createElement('div');
+  resultadoDiv.classList.add('text-center', 'text-white');
+  resultadoDiv.appendChild(nombreCiudad);
+  resultadoDiv.appendChild(actual);
+  resultadoDiv.appendChild(tempMaxima);
+  resultadoDiv.appendChild(tempMin);
+  resultado.appendChild(resultadoDiv);
+}
+
+function kelvinACentigrados(grados) {
+  return parseInt(grados - 273.15);
+}
+
+function limpiarHTML() {
+  while (resultado.firstChild) {
+    resultado.removeChild(resultado.firstChild);
+  }
+}
+
+function Spinner() {
+  limpiarHTML();
+  const divSpinner = document.createElement('div');
+  divSpinner.classList.add('sk-fading-circle');
+  divSpinner.innerHTML = `
+  <div class="sk-circle1 sk-circle"></div>
+  <div class="sk-circle2 sk-circle"></div>
+  <div class="sk-circle3 sk-circle"></div>
+  <div class="sk-circle4 sk-circle"></div>
+  <div class="sk-circle5 sk-circle"></div>
+  <div class="sk-circle6 sk-circle"></div>
+  <div class="sk-circle7 sk-circle"></div>
+  <div class="sk-circle8 sk-circle"></div>
+  <div class="sk-circle99 sk-circle"></div>
+  <div class="sk-circle10 sk-circle"></div>
+  <div class="sk-circle11 sk-circle"></div>
+  <div class="sk-circle12 sk-circle"></div>
+  `;
+
+  resultado.appendChild(divSpinner);
+}
